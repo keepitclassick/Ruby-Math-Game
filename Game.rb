aa5c53c21d22e6ea6c2f8b8618b9db1ddbac24f0
keepitclassick <keepitclassick@gmail.com>
2022-01-03 13:15:06 -0500

    add main and game files with rules

---
+++ Game.rb
@@ -0,0 +1,16 @@
+class Game
+  def initialize
+
+  end
+
+  def start_game
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Are you ready to start the TwO-O-Player Math Game? Let's review the rules."
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Each player has 3 lives, and you lose a life\nwhen the answer is incorrect."
+    puts "----"
+    puts "The game ends when either player loses all their lives."
+    puts "----"
+    puts "If you're ready, type "y" into the prompt to start the game.\n"
+  end
+end
